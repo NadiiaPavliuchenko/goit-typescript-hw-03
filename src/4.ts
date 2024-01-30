7be48713c0177a0eb9c88069dc3d4286542a6052
NadiiaPavliuchenko <n.pavljuchenko@gmail.com>
2024-01-30 16:05:26 +0200

    hw3 pt1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,47 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[];
+
+  constructor(protected key: Key) {
+    this.door = false;
+    this.tenants = [];
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open.");
+    } else {
+      console.log("Incorrect key, door is closed.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +50,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
